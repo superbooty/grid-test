bce3ca6c20815b31b73c0b62a055593b57cca387
Alfonso Guevara <aguevara1@levi.com>
2020-02-16 18:03:19 -0800

    just for fun now

--- src/components/Decorator.vue
+++ src/components/Decorator.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="decorator-content">
+    <div class="decorator-mask">
+    </div>
     <img :src="content.url" />
+    <div class="headline-mobile-align"><h3 data-v-43ca4d66="" class="headline-text">{{content.msg}}</h3></div>
   </div>
 </template>
 
@@ -17,9 +20,32 @@ export default {
 <style scoped>
 .decorator-content {
   height: 100%;
+  position: relative;
 }
 .decorator-content img {
   width: 100%;
   height: 100%;
+  display: block;
 }
+
+.decorator-mask {
+  background-color: #4f4f4f;
+  z-index: 1000;
+  opacity: 0.2;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+}
+
+.headline-mobile-align {
+  position: relative;
+  top: -50%;
+  color: white;
+  height: 0px;
+}
+
+.headline-mobile-align h3 {
+  margin: 0 !important;
+}
+
 </style>
